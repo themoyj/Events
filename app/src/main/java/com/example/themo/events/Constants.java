81d8b56b33ff04e039efa665b34390a0d6c76fec
themoyj <themo@foxmail.com>
2015-10-26 22:22:51 +0800

    我的测试项目库，首次提交。

---
+++ app/src/main/java/com/example/themo/events/Constants.java
@@ -0,0 +1,12 @@
+package com.example.themo.events;
+
+/**
+ * Created by Administrator on 2015/10/26.
+ */
+import android.provider.BaseColumns;
+
+public interface Constants extends BaseColumns{
+    public static final String TABLE_NAME = "events";
+    public static final String TIME = "time";
+    public static final String TITLE = "title";
+}
